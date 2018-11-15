8ebe1b0f84808608cf78bc58d1a0fcb512abac97
Szymon Miazga <szymek26@outlook.com>
2018-11-15 11:34:31 +0100

    Prezentacja danych, dzielenie tablic

---
+++ classification.py
@@ -0,0 +1,30 @@
+# Load libraries
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = pandas.read_csv(url, names=names)
+
+print(dataset.head(20))
+dataset.hist()
+plt.show()
+
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = 0.2
+seed = 6
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
